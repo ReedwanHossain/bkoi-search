9d519dde46813f8950e3fb283fea06be39130408
Badhon <reedwanhossain@gmail.com>
2018-11-16 10:45:10 +0600

    first commit

---
+++ http-service.js
@@ -0,0 +1,20 @@
+myApp.factory('HttpService', function($http, $q){
+  return {
+    post_anything : function(query){
+      console.log(query);
+      var deffered = $q.defer();
+      var data = {
+        'search' : query
+      }
+      $http.post('https://barikoi.xyz/v1/tnt/search/test/', data)
+        .success(function(data){
+          deffered.resolve(data);
+        })
+        .error(function(data, status){
+          deffered.reject(status);
+        });
+
+      return deffered.promise;
+    }
+  }
+});
---
+++ js/app.js
@@ -0,0 +1,114 @@
+var myApp = angular.module("myApp", ['ui.bootstrap', 'leaflet-directive', 'bsLoadingOverlay']);
+myApp.controller("MainController", ['$scope', '$timeout', 'HttpService', 'bsLoadingOverlayService',
+  function($scope, $timeout, HttpService, bsLoadingOverlayService) {
+    var minLength = 0;
+
+     $scope.selected={};
+        // 
+     bsLoadingOverlayService.setGlobalConfig({
+                templateUrl: './templates/loader.html'
+            });
+
+
+    angular.extend($scope, {
+                center: {
+                      lat: 23.757087,
+                      lng: 90.390370,
+                      zoom: 16
+                },
+                defaults: {
+                    zoomAnimation: true,
+                    markerZoomAnimation: true,
+                    fadeAnimation: true
+                },
+
+                markers: {
+                    m1: {
+                        lat: 23.757087,
+                        lng: 90.390370,
+                        draggable: false,
+                    },
+                },
+
+                events: { // or just {} //all events
+                    map: {
+                        enable: ['moveend', 'popupopen'],
+                        logic: 'emit'
+                    },
+                    markers:{
+                      enable: [ 'dragend', 'moveend' ],
+                      logic: 'emit'
+                    }
+                }, layers: {
+                    baselayers: {
+                        
+                        bkoi: {
+                            name: 'barikoi',
+                            url: 'http://map.barikoi.xyz:8080/styles/klokantech-basic/{z}/{x}/{y}.png',
+                            type: 'xyz',
+                            layerOptions: {
+                                attribution: 'Barikoi',
+                                maxZoom: 23
+                            },
+                        },
+                        
+                        
+                        mapbox_light: {
+                             name: 'Mapbox Streets',
+                            url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                            type: 'xyz',
+                            layerOptions: {
+                                apikey: 'pk.eyJ1Ijoicmhvc3NhaW4iLCJhIjoiY2o4Ymt0NndlMHVoMDMzcnd1ZGs4dnJjMSJ9.5Y-mrWQCMXqWTe__0J5w4w',
+                                mapid: 'mapbox.streets',
+                                attribution: 'barikoi',
+                                maxZoom: 23
+                            },
+                            layerParams: {
+                                showOnSelector: true
+                            }
+                        }
+                    }
+                }
+            });
+
+    $scope.onSelect = function(user){
+      $scope.selected = user;
+      console.log(user);
+      $scope.markers.m1.lat = parseFloat($scope.selected.latitude);
+      $scope.markers.m1.lng = parseFloat($scope.selected.longitude)
+      $scope.center.lat = parseFloat($scope.selected.latitude);
+      $scope.center.lng = parseFloat($scope.selected.longitude);
+    };
+    $scope.users = function(userName) {
+      bsLoadingOverlayService.start({
+                referenceId: 'first'
+        });
+     $scope.error_message = '';
+      if (userName.length < minLength) {
+        return [];
+      }
+      $scope.loading = true;
+      return HttpService.post_anything(userName).then(function(data){
+        if (!Array.isArray(data.places)) {
+           $scope.error_message = data.places.Message;
+          bsLoadingOverlayService.stop({
+                        referenceId: 'first'
+                    });
+        }
+        else {
+          $scope.error_message = '';
+          bsLoadingOverlayService.stop({
+                        referenceId: 'first'
+                    });
+        }
+        return data.places;
+      }, function(status){
+        bsLoadingOverlayService.stop({
+                        referenceId: 'first'
+                    });
+      });
+    };
+  }
+]);
+
+
